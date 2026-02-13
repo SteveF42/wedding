9bec69a2a4c5616ae8bf1a18af9ccf2a344ff594
stevef42 <stevewflores43@gmail.com>
2026-02-13 15:44:39 -0800

    changed file path to capital P

---
+++ client/src/routes/Photos/Photos.tsx
@@ -0,0 +1,11 @@
+
+
+const Photos = () => {
+  return (
+    <div className="flex justify-center">
+      <h1>Check back to upload photos during the reception!</h1>
+    </div>
+  )
+}
+
+export default Photos
